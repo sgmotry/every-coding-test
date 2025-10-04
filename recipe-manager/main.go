fd2abb490ec5eaa4bfefd4cd56f14bb619d257e7
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-04 18:07:02 +0900

    データ読み込みテスト用コード記述

--- recipe-manager/main.go
+++ recipe-manager/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"every-coding-test/internal/data"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("aaa")
+	data, err := data.GetRecipes()
+
+	if err != nil {
+		fmt.Println("Error while getting recipes.\n", err)
+	}
+	fmt.Println(data[0].Category)
 }
