9fd7fae6ad36494ca74faf2190aa3cbd18b2b4c0
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-04 18:06:17 +0900

    データ構造にjson用のフィールドを定義

--- recipe-manager/internal/model/model.go
+++ recipe-manager/internal/model/model.go
@@ -2,33 +2,33 @@ package model
 
 func NewRecipe(id int, name string, category string, howmany int, time int, descript []string, ingredient []Ingredient, nutrition []Nutrition) Recipe {
 	return Recipe{
-		id:          id,
-		name:        name,
-		category:    category,
-		howmany:     howmany,
-		time:        time,
-		descript:    descript,
-		ingredients: ingredient,
-		nutrition:   nutrition,
+		Id:          id,
+		Name:        name,
+		Category:    category,
+		Howmany:     howmany,
+		Time:        time,
+		Descript:    descript,
+		Ingredients: ingredient,
+		Nutrition:   nutrition,
 	}
 }
 
 type Recipe struct {
-	id          int          // 識別用ID（プライマリキー）
-	name        string       // レシピ名
-	category    string       // 分類
-	howmany     int          // 何人分か
-	time        int          // 調理にかかる時間
-	descript    []string     // 作り方の説明
-	ingredients []Ingredient // 材料
-	nutrition   []Nutrition  // 栄養価
+	Id          int          `json:"id"`          // 識別用ID（プライマリキー）
+	Name        string       `json:"name"`        // レシピ名
+	Category    string       `json:"category"`    // 分類
+	Howmany     int          `json:"howmany"`     // 何人分か
+	Time        int          `json:"time"`        // 調理にかかる時間
+	Descript    []string     `json:"descript"`    // 作り方の説明
+	Ingredients []Ingredient `json:"ingredients"` // 材料
+	Nutrition   []Nutrition  `json:"nutrition"`   // 栄養価
 }
 
 type Ingredient struct {
-	name   string
-	amount string
+	Name   string `json:"name"`
+	Amount string `json:"amount"`
 }
 type Nutrition struct {
-	name   string
-	amount string
+	Name   string `json:"name"`
+	Amount string `json:"amount"`
 }
