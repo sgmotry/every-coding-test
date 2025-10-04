bef8857e2af2182ba308d37f6bb651916f3f9d9f
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-04 18:06:43 +0900

    ローカルファイルのjsonからデータを読み込んで返す関数を実装

--- recipe-manager/internal/data/loader.go
+++ recipe-manager/internal/data/loader.go
@@ -1,1 +1,21 @@
 package data
+
+import (
+	"encoding/json"
+	"every-coding-test/internal/model"
+	"os"
+)
+
+func GetRecipes() ([]model.Recipe, error) {
+	bytes, err := os.ReadFile("recipes.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var recipeData []model.Recipe
+	if err := json.Unmarshal(bytes, &recipeData); err != nil {
+		return nil, err
+	}
+
+	return recipeData, err
+}
