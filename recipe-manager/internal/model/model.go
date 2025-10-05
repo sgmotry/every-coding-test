217fe117f48c089a43ab98b261bdab9417e2d472
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-05 16:22:21 +0900

    データ構造の変更とそれに伴うテストデータの変更

--- recipe-manager/internal/model/model.go
+++ recipe-manager/internal/model/model.go
@@ -1,6 +1,6 @@
 package model
 
-func NewRecipe(id int, name string, category string, howmany int, time int, descript []string, ingredient []Ingredient, nutrition []Nutrition) Recipe {
+func NewRecipe(id uint32, name string, category string, howmany int, time Amount, descript []string, ingredient []Ingredient, nutrition Nutrition) Recipe {
 	return Recipe{
 		Id:          id,
 		Name:        name,
@@ -14,21 +14,39 @@ func NewRecipe(id int, name string, category string, howmany int, time int, desc
 }
 
 type Recipe struct {
-	Id          int          `json:"id"`          // 識別用ID（プライマリキー）
+	Id          uint32       `json:"id"`          // 識別用ID（プライマリキー）
 	Name        string       `json:"name"`        // レシピ名
 	Category    string       `json:"category"`    // 分類
 	Howmany     int          `json:"howmany"`     // 何人分か
-	Time        int          `json:"time"`        // 調理にかかる時間
+	Time        Amount       `json:"time"`        // 調理にかかる時間
 	Descript    []string     `json:"descript"`    // 作り方の説明
 	Ingredients []Ingredient `json:"ingredients"` // 材料
-	Nutrition   []Nutrition  `json:"nutrition"`   // 栄養価
+	Nutrition   Nutrition    `json:"nutrition"`   // 栄養価
+}
+
+type Amount struct {
+	Number float32 `json:"number"` // 数値
+	Unit   string  `json:"unit"`   // 単位
+}
+
+type Descript struct {
+	Text string `json:"text"`
+	// Image string `json:"image"`
 }
 
 type Ingredient struct {
 	Name   string `json:"name"`
-	Amount string `json:"amount"`
+	Amount Amount `json:"amount"`
 }
+
 type Nutrition struct {
+	Calorie        Amount           `json:"calorie"`
+	Protein        Amount           `json:"protein"`
+	Lipids         Amount           `json:"lipids"`
+	Carbohydrates  Amount           `json:"carbohydrates"`
+	OtherNutrition []OtherNutrition `json:"otherNutrition"`
+}
+type OtherNutrition struct {
 	Name   string `json:"name"`
-	Amount string `json:"amount"`
+	Amount Amount `json:"amount"`
 }
