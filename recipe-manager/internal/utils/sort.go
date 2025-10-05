336b61a56e884e4d8a3f06debd1472e95c498ed0
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-05 17:36:23 +0900

    ソート関数の引数のチェック処理実装

--- recipe-manager/internal/utils/sort.go
+++ recipe-manager/internal/utils/sort.go
@@ -1,1 +1,52 @@
 package utils
+
+import (
+	"every-coding-test/internal/model"
+	"fmt"
+)
+
+type OrderByOptions string
+
+const (
+	Id          OrderByOptions = "id"
+	Name        OrderByOptions = "name"
+	Calories    OrderByOptions = "calories"
+	CookingTime OrderByOptions = "cookingTime"
+)
+
+func validateOrderBy(orderBy OrderByOptions) error {
+	switch orderBy {
+	case Id, Name, Calories, CookingTime:
+		return nil
+	default:
+		return fmt.Errorf("'%s' is invalid orderby option", orderBy)
+	}
+}
+
+type OrderOptions string
+
+const (
+	Asc  OrderOptions = "asc"
+	Desc OrderOptions = "desc"
+)
+
+func validateOrder(order OrderOptions) error {
+	switch order {
+	case Asc, Desc:
+		return nil
+	default:
+		return fmt.Errorf("'%s' is invalid order option", order)
+	}
+}
+
+func SortRecipe(recipe []model.Recipe, orderBy OrderByOptions, order OrderOptions) []model.Recipe {
+	orderByErr := validateOrderBy(orderBy)
+	if orderByErr != nil {
+		fmt.Println("Error while checking orderBy.\n", orderByErr)
+	}
+	orderErr := validateOrder(order)
+	if orderErr != nil {
+		fmt.Println("Error while checking order.\n", orderErr)
+	}
+	return recipe
+}
