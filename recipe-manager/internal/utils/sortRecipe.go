5ff31dc9ae3b354e9ad28f7056862926ed726c14
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-10 02:04:48 +0900

    エラーログの文言変更

--- recipe-manager/internal/utils/sortRecipe.go
+++ recipe-manager/internal/utils/sortRecipe.go
@@ -49,11 +49,11 @@ func validateOrder(order OrderOptions) error {
 func SortRecipe(recipes []model.Recipe, orderBy OrderByOptions, order OrderOptions) ([]model.Recipe, error) {
 	orderByErr := validateOrderBy(orderBy)
 	if orderByErr != nil {
-		return nil, fmt.Errorf("error while checking orderBy.\n%e", orderByErr)
+		return nil, fmt.Errorf("error while checking orderBy.\n%s", orderByErr)
 	}
 	orderErr := validateOrder(order)
 	if orderErr != nil {
-		return nil, fmt.Errorf("error while checking order.\n%e", orderErr)
+		return nil, fmt.Errorf("error while checking order.\n%s", orderErr)
 	}
 
 	sortedRecipe := quickSort(recipes, orderBy, order)
