49db3de7965fa099c3729b91cd462e3bf206c368
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-09 19:27:29 +0900

    ナップサック問題・DPテーブルの初期化を実装

--- recipe-manager/internal/utils/knapsack.go
+++ recipe-manager/internal/utils/knapsack.go
@@ -2,6 +2,15 @@ package utils
 
 import "every-coding-test/internal/model"
 
-func Knapsack(data []model.Recipe, maxCalories int, maxCookingTime int) []uint32 {
+func Knapsack(recipes []model.Recipe, maxCalories int, maxCookingTime int) []uint32 {
+	recipesLen := len(recipes)
+
+	dp := make([][][]int, recipesLen+1)
+	for i := range dp {
+		dp[i] = make([][]int, maxCalories+1)
+		for j := range dp[i] {
+			dp[i][j] = make([]int, maxCookingTime+1)
+		}
+	}
 	return []uint32{}
 }
