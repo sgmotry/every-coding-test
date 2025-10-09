8fa9908dc4736b21b742ea9cad8511a4dc229d7c
sgmotry <45906970+sgmotry@users.noreply.github.com>
2025-10-09 21:53:51 +0900

    ナップサック問題用関数実装

--- recipe-manager/internal/utils/knapsack.go
+++ recipe-manager/internal/utils/knapsack.go
@@ -1,8 +1,10 @@
 package utils
 
-import "every-coding-test/internal/model"
+import (
+	"every-coding-test/internal/model"
+)
 
-func Knapsack(recipes []model.Recipe, maxCalories int, maxCookingTime int) []uint32 {
+func Knapsack(recipes []model.Recipe, maxCalories int, maxCookingTime int) ([]uint32, float32) {
 	recipesLen := len(recipes)
 
 	dp := make([][][]int, recipesLen+1)
@@ -12,5 +14,46 @@ func Knapsack(recipes []model.Recipe, maxCalories int, maxCookingTime int) []uin
 			dp[i][j] = make([]int, maxCookingTime+1)
 		}
 	}
-	return []uint32{}
+
+	for i := 1; i <= recipesLen; i++ {
+		recipe := recipes[i-1]
+		calorie := int(recipe.Nutrition.Calorie)
+		time := recipe.Time
+		protein := int(recipe.Nutrition.Protein * 10)
+
+		for c := 0; c <= maxCalories; c++ {
+			for t := 0; t <= maxCookingTime; t++ {
+				dp[i][c][t] = dp[i-1][c][t]
+
+				if c >= calorie && t >= time {
+					withRecipe := dp[i-1][c-calorie][t-time] + protein
+					if withRecipe > dp[i][c][t] {
+						dp[i][c][t] = withRecipe
+					}
+				}
+			}
+		}
+	}
+
+	selectedRecipesId := []uint32{}
+	c := maxCalories
+	t := maxCookingTime
+
+	for i := recipesLen; i > 0; i-- {
+		recipe := recipes[i-1]
+		calorie := int(recipe.Nutrition.Calorie)
+		time := recipe.Time
+		protein := int(recipe.Nutrition.Protein * 10)
+
+		// レシピが選ばれているかチェック
+		if c >= calorie && t >= time {
+			if dp[i][c][t] == dp[i-1][c-calorie][t-time]+protein && dp[i][c][t] > dp[i-1][c][t] {
+				selectedRecipesId = append([]uint32{recipe.Id}, selectedRecipesId...)
+				c -= calorie
+				t -= time
+			}
+		}
+	}
+
+	return selectedRecipesId, float32(dp[recipesLen][maxCalories][maxCookingTime]) / 10
 }
